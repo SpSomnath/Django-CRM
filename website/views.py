4542e79c193958a15300f185429ca8753544cefc
spsomnath <spsomnath100@gmail.com>
2023-08-09 22:39:20 +0530

    'register'

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .form import SignUpForm
 
 # Create your views here.
 
@@ -17,6 +18,7 @@ def home(request):
             return redirect('home')
         else:
             messages.success(request, "There is Error in login please try again")
+            return redirect('home')
     else:
         return render(request,'home.html', {})
    
@@ -26,3 +28,23 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out...")
     return redirect('home')
+
+
+
+def register_user(request):
+    if request.method =='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # authenticate and login 
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password = password)
+            login(request, user)
+            messages.success(request, " You have succesfully logedin! Welcome")
+            return redirect('home') 
+    else:
+        form = SignUpForm()    
+        return render(request, 'register.html',{'form':form})
+    
+    return render(request, 'register.html',{'form':form})
