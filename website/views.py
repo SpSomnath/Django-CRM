a2ec83bdc3340f1f93427f67875941eee6f035ea
spsomnath <spsomnath100@gmail.com>
2023-08-10 22:03:47 +0530

    'issu-in-record-creation'

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .form import SignUpForm
+from .form import SignUpForm, AddRecord
 from .models import Record
 
 # Create your views here.
@@ -55,8 +55,33 @@ def register_user(request):
 def customer_record(request,pk):
     if request.user.is_authenticated:
         # look Up Record 
-        customer_record = Record.Objects.get(id=pk)
+        customer_record = Record.objects.get(id=pk)
         return render(request, 'record.html',{'customer_record':customer_record})
     else: 
         messages.success(request, "You must be Logged in to view that page..")
         return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, " Record Deleted Sucessfully ")
+        return redirect('home')
+    else: 
+        messages.success(request," You Must Be Logged In ")
+        return redirect('home')
+    
+
+
+def add_record(request):
+	form = AddRecord(request.POST or None)
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			if form.is_valid():
+				add_record = form.save()
+				messages.success(request, "Record Added...")
+				return redirect('home')
+		return render(request, 'add_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In...")
+		return redirect('home')
